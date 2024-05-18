9ac34ab7378e3accceb41f4c36d7a30c9af7d46d
smcon <165791594+smcon@users.noreply.github.com>
2024-05-18 21:53:26 +0900

    Add files via upload

--- 悠希工業/script.js
+++ 悠希工業/script.js
@@ -13,5 +13,19 @@ $(function() {
     $(this).stop().addClass('slide-left');
   }
   });
-
+  //スクロールをなめらかにする
+  $('a[href^="#"]').click(function () {
+    const speed = 500;
+    const href = $(this).attr('href');
+    let $target;
+    if (href == '#') {
+      $target = $('html');
+    }
+    else {
+      $target = $(href);
+    }
+    const position = $target.offset().top;
+    $('html, body').animate({ 'scrollTop': position }, speed, 'swing');
+    return false;
+  });
 });
